761b1d64fca98ba957a696b77d1e5069bebb69d1
Brandon Tsang <brandononline2@gmail.com>
2022-12-03 20:27:14 -0500

    add overflow: hidden to switcher layout

--- components/ShaderLayout/switcher-mode/SwitcherLayout.tsx
+++ components/ShaderLayout/switcher-mode/SwitcherLayout.tsx
@@ -21,7 +21,7 @@ const SwitcherLayout: FC<Props> = ({children}) => {
 	const itemSeparation = itemWidth * 0.4
 
 	return (
-		<div className="flex h-full w-full items-center">
+		<div className="flex h-full w-full items-center overflow-hidden">
 			<motion.div
 				className="h-[min(50vw,30rem)] w-full [perspective:1500px]"
 				onPointerDown={(e) => void controls.start(e)}
